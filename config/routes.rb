733cb565ac7cc23c64edcaca2f93fa75da07430a
Seio Inoue <sei0okun@gmail.com>
2025-03-29 23:17:32 +0900

    use form_with for CSRF validations

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   resources :pages
-  post "/pages/:id/analyze", to: "pages#analyze", as: :page_analyze
+  post "/pages/:id/analyze", to: "pages#analyze", as: :analyze
   get "/view", to: "pages#view", as: :view
 
   get "/about", to: "pages#about", as: :about
